d627eae00e283de44b47c2d35509c6fcad970a76
tommy stear <tom.stear@bodybuilding.com>
2016-03-30 18:13:26 -0600

    Completed functionality

--- 03-30/geojson.py
+++ 03-30/geojson.py
@@ -1,4 +1,5 @@
 import urllib
+import urllib.request, urllib.parse, urllib.error
 import json
 
 serviceurl = 'http://maps.googleapis.com/maps/api/geocode/json?'
@@ -13,7 +14,10 @@ def getAddressToSearch():
     Ask user for input, like "Boise,ID"
     @return (str) The user's input
     """
-    pass
+    print('Please enter a location to search for (e.g. "Boise, ID"):  ')
+    address = input()
+#    print(address)
+    return address
 
 
 def buildRequest(addr):
@@ -27,7 +31,10 @@ def buildRequest(addr):
     MUST BE URLENCODED (urllib.urlencode())
     @return (str) the request document
     """
-    pass
+    document = {'sensor':'false','address':'%s'%(addr)}
+#    print(document)
+#    print(json.dumps(document))
+    return serviceurl + urllib.parse.urlencode(document)
 
 
 def readResponseDocument(req):
@@ -37,16 +44,22 @@ def readResponseDocument(req):
     @return (str) The JSON data in the response
     @raises (ValueError) If the request fails
     """
-    pass
+    resp = urllib.request.urlopen(req)
+#    print(resp)
+    print('HTTP %d:  %s' % (resp.getcode(), resp.reason))
+    respData = resp.read()
+#    print(respData)
+    return respData
 
 
 def printResponseDocument(resp):
     """
     Using the JSON parser (import json), read the fields from 
-    the data document in the response.  Print only the data docment, 
+    the data document in the response.  Print only the data document, 
     not the entire response.
     """
-    pass
+    respObject = json.loads(resp.decode("utf-8"))
+    print('printResponseDocument',respObject)
 
 
 def printCoords(resp):
@@ -54,29 +67,28 @@ def printCoords(resp):
     Using the JSON parser, find the Latitude and Longitude 
     values in the response, and print them nicely.
     """
-    pass
+    respObject = json.loads(resp.decode("utf-8"))
+    lat = respObject['results'][0]['geometry']['location']['lat']
+    lng = respObject['results'][0]['geometry']['location']['lng']
+    print('Latitude: %f, Longitude: %f' % (lat,lng))
 
 
 def printLocation(resp):
     """
     Using the JSON parser, find the "formatted_address" field.
     """
-    pass
-
+    respObject = json.loads(resp.decode("utf-8"))
+    formatted_address = respObject['results'][0]['formatted_address']
+    print('Formatted Address:  %s' % formatted_address)
 
 
 if __name__ == "__main__":
     address = getAddressToSearch()
     req = buildRequest(address)
-    
-    print('Retrieving...', url)
-    
+    print('Retrieving...', req)
     resp = readResponseDocument(req)
-    
     printResponseDocument(resp)
-    
     printCoords(resp)
-    
     printLocation(resp)
     
     
